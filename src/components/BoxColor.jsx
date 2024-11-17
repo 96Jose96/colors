b7659eba6973957b5ae2e6a852f451f6f0106b35
Jose <jm.diaz6620@gmail.com>
2024-11-17 17:02:04 +0100

    terminado

--- src/components/BoxColor.jsx
+++ src/components/BoxColor.jsx
@@ -1,7 +1,26 @@
-function boxColor ({color, value}) {
+const boxColor = ({ color, colors }) => {
+  
+  const boxStyle = (borderColor) => {
+    return borderColor === color ? borderColor : ''
+  }
+
+  const chageText = (borderColor) => {
+    const text = borderColor === color ? `Si, soy el color ${color}` : `No soy el color ${color}`
+
+    return (
+      <span style={{ color: borderColor === color ? 'black' : borderColor, fontWeight: 'bold', fontSize: '25px' }}  >{text}</span>
+    )
+  }
+
   return (
     <>
+      {colors.map((borderColor, index) => (
+        <div key={index}  className={`box ${borderColor}`} style={{ borderColor: borderColor, backgroundColor: boxStyle(borderColor)}}>
+          {chageText(borderColor)}
+        </div>
+      ))}
     </> 
-  )
+  )    
 }
+
 export default boxColor;  
